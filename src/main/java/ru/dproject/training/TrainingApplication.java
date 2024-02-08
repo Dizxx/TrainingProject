ebf4080ff30d3e28e030baa69605f8824f0273b6
Dizxx <steam090909@mail.ru>
2024-02-08 13:10:53 +0500

    Init commit

---
+++ src/main/java/ru/dproject/training/TrainingApplication.java
@@ -0,0 +1,13 @@
+package ru.dproject.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dproject/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dproject.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
